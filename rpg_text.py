96cd41ff301c0c1ac90feb8e56158c6c622145ec
R4WD4WG <ruari999@gmail.com>
2023-04-30 23:48:31 -0500

    Add files via upload

--- rpg_text.py
+++ rpg_text.py
@@ -7,30 +7,28 @@ def submit_message():
     # Get the user input
     message = text_box2.get("1.0", "end-1c")
 
-    # Clear the input text box
-    text_box2.delete("1.0", "end")
+    # Check if the input is not empty
+    if message.strip():
+        # Clear the input text box
+        text_box2.delete("1.0", "end")
 
-    # Format the message with the player name
-    formatted_message = "Player: " + message + "\n"
+        # Format the message with the player name
+        formatted_message = "Player: " + message + "\n"
 
-    # Enable the read-only text box to insert the message
-    text_box1.config(state="normal")
-    text_box1.insert("end", formatted_message)
+        # Enable the read-only text box to insert the message
+        text_box1.config(state="normal")
+        text_box1.insert("end", formatted_message)
 
-    # Disable the read-only text box again
-    text_box1.config(state="disabled")
+        # Disable the read-only text box again
+        text_box1.config(state="disabled")
 
 
 # Display Message function
 def display_message(message):
-    formatted_message = message + "\n"
-
-    # Enable the read-only text box to insert the message
-    text_box1.config(stat="normal")
-    text_box1.insert("end", formatted_message)
-
-    # Disable the read-only text box again
+    text_box1.config(state="normal")
+    text_box1.insert("end", message + "\n")
     text_box1.config(state="disabled")
+    text_box1.yview("end")
 
 
 # Display Delayed Message function
@@ -39,6 +37,34 @@ def delayed_display_message(message, delay=1000):
     root.after(delay, display_message, message)
     # Default is 1000ms (1 second)
     # Changeable when called
+
+
+# Set Player Name function
+def set_player_name():
+    global player_name
+    player_name = text_box2.get("1.0", "end").strip()
+    text_box2.delete("1.0", "end")
+    display_message("Your name is " + player_name + "?")
+    display_message("1. Yes")
+    display_message("2. No")
+    button.config(command=confirm_player_name)
+
+
+# Confirm Player Name function
+def confirm_player_name():
+    message = text_box2.get("1.0", "end-1c").strip()
+    if message == "1":
+        text_box2.delete("1.0", "end")
+        display_message("Welcome to the game, " + player_name + "!")
+        button.config(command=submit_message)
+    elif message == "2":
+        text_box2.delete("1.0", "end")
+        display_message("Enter your name below and press submit to begin.")
+        button.config(command = set_player_name)
+    else:
+        text_box2.delete("1.0", "end")
+        display_message("Please enter either 1 for Yes or 2 for No.")
+        button.config(command = confirm_player_name)
 
 
 # Create the main window
@@ -59,7 +85,7 @@ text_box2 = tk.Text(bottom_frame, height=int(0.25 * 600 / 17), width=int(0.75 * 
 text_box2.pack(side="left", fill="both", expand=True, padx=5, pady=5)
 
 # Create the button taking up 25% of the screen height, 25% of the width, anchored South East
-button = tk.Button(bottom_frame, text="Submit", height=int(0.25 * 600 / 17), width=int(0.25 * 800 / 8), command=submit_message)
+button = tk.Button(bottom_frame, text="Submit", height=int(0.25 * 600 / 17), width=int(0.25 * 800 / 8), command=set_player_name)
 button.pack(side="right", padx=5, pady=5)
 
 # Create the player class
@@ -145,6 +171,4 @@ submit_message()
 
 # Run the main loop
 root.mainloop()
-
-
 
